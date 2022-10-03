a1d699ab6ce186b142a4908ab816ed85f22f671b
Bemoreiray <besilva557@gmail.com>
2022-10-03 11:43:17 -0300

    aula-05 e pdfs

--- aula-05/aplicacaoAula/src/app/app.component.ts
+++ aula-05/aplicacaoAula/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'aply-root',
+  selector: 'bia-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'SENAC';
+  alunos = {nome: 'João Silva', aprovado: true};
+  
 }
